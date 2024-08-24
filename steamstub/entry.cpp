5dbe61d4f691340108d9332dddec07586650c13f
mopem <mopem.sucks@proton.me>
2024-08-24 22:11:10 +0200

    add debug tracing to steamstub

--- steamstub/entry.cpp
+++ steamstub/entry.cpp
@@ -1,12 +1,22 @@
-#include "steam_api.h"
-
 #include <Windows.h>
+
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/spdlog.h>
 
 BOOL WINAPI DllMain(HINSTANCE instance_handle, DWORD reason, LPVOID _reserved) {
   switch (reason) {
   case DLL_PROCESS_ATTACH:
+    try {
+      auto logger = spdlog::basic_logger_mt("t6_base", "t6_base.log");
+      spdlog::set_level(spdlog::level::debug);
+      logger->debug("DllMain_Process_Attach");
+    } catch (const std::exception &e) {
+      MessageBoxA(nullptr, "ERROR", "Failed to allocate T6Base logger", MB_OK);
+      _Exit(1);
+    }
     break;
   case DLL_PROCESS_DETACH:
+    spdlog::drop_all();
     break;
   }
   return TRUE;
--- steamstub/stub.cpp
+++ steamstub/stub.cpp
@@ -1,5 +1,7 @@
 #include <stdint.h>
 
+#include <spdlog/spdlog.h>
+
 #ifdef _WIN32
 #define T6_BASE_STEAM_API extern "C" __declspec(dllexport)
 #else
@@ -33,51 +35,111 @@ class ISteamUtils005;
 
 class CallbackBase;
 
-T6_BASE_STEAM_API bool SteamAPI_Init() { return true; }
+T6_BASE_STEAM_API bool SteamAPI_Init() {
+  spdlog::get("t6_base")->debug("SteamAPI_Init");
+  spdlog::get("t6_base")->flush();
+  return true;
+}
 
 T6_BASE_STEAM_API void
 SteamAPI_RegisterCallResult(CallbackBase *callback,
-                            SteamAPICall api_call_handle) {}
+                            SteamAPICall api_call_handle) {
+  spdlog::get("t6_base")->debug("SteamAPI_RegisterCallResult");
+  spdlog::get("t6_base")->flush();
+}
 
 T6_BASE_STEAM_API void SteamAPI_RegisterCallback(CallbackBase *callback,
-                                                 int callback_id) {}
+                                                 int callback_id) {
+  spdlog::get("t6_base")->debug("SteamAPI_RegisterCallback");
+  spdlog::get("t6_base")->flush();
+}
 
 T6_BASE_STEAM_API bool SteamAPI_RestartAppIfNecessary(uint32_t self_app_id) {
+  spdlog::get("t6_base")->debug("SteamAPI_RestartAppIfNecessary");
+  spdlog::get("t6_base")->flush();
   return false;
 }
 
-T6_BASE_STEAM_API void SteamAPI_RunCallbacks() {}
+T6_BASE_STEAM_API void SteamAPI_RunCallbacks() {
+  spdlog::get("t6_base")->debug("SteamAPI_RunCallbacks");
+  spdlog::get("t6_base")->flush();
+}
 
-T6_BASE_STEAM_API void SteamAPI_Shutdown() {}
+T6_BASE_STEAM_API void SteamAPI_Shutdown() {
+  spdlog::get("t6_base")->debug("SteamAPI_Shutdown");
+  spdlog::get("t6_base")->flush();
+}
 
 T6_BASE_STEAM_API void
 SteamAPI_UnregisterCallResult(CallbackBase *callback,
-                              SteamAPICall api_call_handle) {}
+                              SteamAPICall api_call_handle) {
+  spdlog::get("t6_base")->debug("SteamAPI_UnregisterCallResult");
+  spdlog::get("t6_base")->flush();
+}
 
-T6_BASE_STEAM_API void SteamAPI_UnregisterCallback(CallbackBase *callback) {}
+T6_BASE_STEAM_API void SteamAPI_UnregisterCallback(CallbackBase *callback) {
+  spdlog::get("t6_base")->debug("SteamAPI_UnregisterCallback");
+  spdlog::get("t6_base")->flush();
+}
 
-T6_BASE_STEAM_API ISteamApps005 *SteamApps() { return nullptr; }
+T6_BASE_STEAM_API ISteamApps005 *SteamApps() {
+  spdlog::get("t6_base")->debug("SteamApps");
+  spdlog::get("t6_base")->flush();
+  return nullptr;
+}
 
-T6_BASE_STEAM_API ISteamFriends013 *SteamFriends() { return nullptr; }
+T6_BASE_STEAM_API ISteamFriends013 *SteamFriends() {
+  spdlog::get("t6_base")->debug("SteamFriends");
+  spdlog::get("t6_base")->flush();
+  return nullptr;
+}
 
-T6_BASE_STEAM_API ISteamGameServer011 *SteamGameServer() { return nullptr; }
+T6_BASE_STEAM_API ISteamGameServer011 *SteamGameServer() {
+  spdlog::get("t6_base")->debug("SteamGameServer");
+  spdlog::get("t6_base")->flush();
+  return nullptr;
+}
 
 T6_BASE_STEAM_API bool
 SteamGameServer_Init(uint32_t ip, uint16_t port, uint16_t game_port,
                      uint16_t spectator_port, uint16_t query_port,
                      SteamServerMode server_mode, const char *game_dir,
                      const char *version_str) {
+  spdlog::get("t6_base")->debug("SteamGameServer_Init");
+  spdlog::get("t6_base")->flush();
   return true;
 }
 
-T6_BASE_STEAM_API void SteamGameServer_RunCallbacks() {}
+T6_BASE_STEAM_API void SteamGameServer_RunCallbacks() {
+  spdlog::get("t6_base")->debug("SteamGameServer_RunCallbacks");
+  spdlog::get("t6_base")->flush();
+}
 
-T6_BASE_STEAM_API void SteamGameServer_Shutdown() {}
+T6_BASE_STEAM_API void SteamGameServer_Shutdown() {
+  spdlog::get("t6_base")->debug("SteamGameServer_Shutdown");
+  spdlog::get("t6_base")->flush();
+}
 
-T6_BASE_STEAM_API ISteamNetworking005 *SteamNetworking() { return nullptr; }
+T6_BASE_STEAM_API ISteamNetworking005 *SteamNetworking() {
+  spdlog::get("t6_base")->debug("SteamNetworking");
+  spdlog::get("t6_base")->flush();
+  return nullptr;
+}
 
-T6_BASE_STEAM_API ISteamUser016 *SteamUser() { return nullptr; }
+T6_BASE_STEAM_API ISteamUser016 *SteamUser() {
+  spdlog::get("t6_base")->debug("SteamUser");
+  spdlog::get("t6_base")->flush();
+  return nullptr;
+}
 
-T6_BASE_STEAM_API ISteamUserStats011 *SteamUserStats() { return nullptr; }
+T6_BASE_STEAM_API ISteamUserStats011 *SteamUserStats() {
+  spdlog::get("t6_base")->debug("SteamUserStats");
+  spdlog::get("t6_base")->flush();
+  return nullptr;
+}
 
-T6_BASE_STEAM_API ISteamUtils005 *SteamUtils() { return nullptr; }
+T6_BASE_STEAM_API ISteamUtils005 *SteamUtils() {
+  spdlog::get("t6_base")->debug("SteamUtils");
+  spdlog::get("t6_base")->flush();
+  return nullptr;
+}
